d92b70f1bf8dad6257902c56f17bdbeba5022a99
saleh2001k <saleh.ayman2001@gmail.com>
2023-05-02 07:24:01 +0300

    Solving 3Qs of CC17

---
+++ code-challenges/challenges-17.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// referring to the given examples find out the pattern used and
+// Write a function that takes 2 integers and prints the patterns using recursion
+//
+// Input: 16, 5
+// Output: [16, 11, 6, 1, -4, 1, 6, 11, 16]
+//
+// Input: 50, 9
+// Output: [50, 41, 32, 23, 14, 5, -4, 5, 14, 23, 32, 41, 50]
+//
+
+const recursionPattern = (int1, int2) => {
+  const pattern = [int1];
+
+  const recursionHelper = (num, diff, add) => {
+    if (num <= 0) {
+      add = true;
+    }
+
+    if (num === int1 && add) {
+      return pattern;
+    }
+
+    if (add) {
+      pattern.push(num + diff);
+      return recursionHelper(num + diff, diff, add);
+    } else {
+      pattern.push(num - diff);
+      return recursionHelper(num - diff, diff, add);
+    }
+  };
+
+  recursionHelper(int1, int2, false);
+  return pattern;
+};
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes a string (HTML tag)
+// and extracts the link from the HTML tag
+//
+// Ex: <a href="http://www.hackerrank.com"><h1><b>HackerRank</b></h1></a> ==> "www.hackerrank.com"
+// Ex: <a href="http://www.something.org"><h1><b>HackerRank</b></h1></a> ==> "www.something.org"
+//
+// Note:
+//  Assume that links end with .com, .org or .net
+//
+
+const filterLinks = (str) => {
+  const regex = /href=["'](?:https?:\/\/)?([^\s"'<>]+).(com|org|net)["']/;
+  const match = str.match(regex);
+  return match ? match[1] + "." + match[2] : null;
+};
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Required:
+//
+// A phrase is considered palindrome if, after converting all uppercase letters into lowercase letters
+// and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+//
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// as you can see "amanaplanacanalpanama" is a palindrome.
+//
+
+const isPalindrome = (str) => {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+};
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+//
+//  Write a function that takes 2 arguments, one is a string and the other is an array
+//  the function should return a boolean than indicates if both have the same pattern
+//
+//  EX:
+//  Input: "abba", ['cat', 'dog', 'dog', 'cat']
+//  output: true
+//
+//  as you can see the a acted as the cat in the array, and b as the dog, and the pattern was the same
+//
+//  Input: "cda", ['cat', 'dog', 'cat']
+//  output: false
+//
+//  here if the pattern had three different things so the array should had 3 different things to be true
+//
+
+const samePattern = (str, arr) => {
+  // write your code here
+};
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { recursionPattern, filterLinks, isPalindrome, samePattern };
